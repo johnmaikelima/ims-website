fda04402c7617391093f7acf74c48ba61ed69714
johnmaikelima <johnpatrocinio1@gmail.com>
2025-07-03 23:37:32 -0300

    fix: Atualiza URL do logo para o endereço correto

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -5,7 +5,7 @@ export default function Logo() {
   return (
     <Link href="/" className="flex items-center">
       <FallbackImage
-        src="https://imsmanutencao.com.br/wp-content/uploads/2025/01/imagem_2025-01-13_123646591.png"
+        src="https://site.imsmanutencao.com.br/wp-content/uploads/2025/01/imagem_2025-01-13_123646591-768x472.png"
         alt="IMS Manutenção Logo"
         width={240}
         height={64}
